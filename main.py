975dfa86c63aba3e05af3521f8c22aa05a459fbd
nhoj98 <rustydog323@gmail.com>
2024-11-09 14:36:39 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import random
+import random, pickle
 
 
 edges = []
@@ -6,20 +6,67 @@ nodes = []
 cars = []
 
 mapsize = [10,10]
+inverse_directions = {"u":"d","d":"u","l":"r","r":"l"}
+
+def node_by_pos(x,y):
+    for n in nodes:
+        if n.x == x and n.y == y:
+            return n
+    return False
+
+def find_next_node(node, d):
+    x,y = node.x,node.y
+    if d in "ud":
+        while y>=0 and y<mapsize[1]:
+            if d == "u":
+                y-=1
+            else:
+                y+=1
+            if new_node := node_by_pos(x,y):
+                return new_node
+    if d in "lr":
+        while y>=0 and y<mapsize[1]:
+            if d == "l":
+                y-=1
+            else:
+                y+=1
+            if new_node := node_by_pos(x,y):
+                return new_node
+    return False
+        
+def get_neighbor (node):
+    #returns list of tuples. form [(node, dir), (node,dir)]
+    out = []
+
+    out.append((find_next_node(node, 'u'), 'u'))
+    out.append((find_next_node(node, 'r'), 'r'))
+    out.append ((find_next_node(node, 'd'), 'd'))
+    out.append((find_next_node(node, 'l'), 'l'))
+    
+    result = []
+    for i in range (len(out)):
+        if not( out[i][0] == False or out[i][0] == None):
+            result.append(out[i])
+    return result
+
 
 
 class node():
-    edges={"u":None,"d":None,"l":None,"r":None}
-    lightud=False
-    cars_in_intersection = []
     def __init__(self,pos): #pos is a list or tuple of length 2 (x,y)
+        self.lightud=False
+        self.cars_in_intersection = []
+        self.edges={"u":None,"d":None,"l":None,"r":None}
         self.x,self.y=pos
         
     def move_car(self):
         pass
+        
+    def __str__(self):
+        return str(self.edges)+"\n"+str(self.lightud)+"\n"+str(self.cars_in_intersection)+"\n"+str(f"({self.x},{self.y})")
 
 class car():
     speed = 0
+    position = 0
     def pathfind(self):
         pass #self.path = list of nodes we want to get to
     
@@ -28,22 +75,38 @@ class car():
         
 
 class edge():
-    def __init__(self, carsP, carsN, nodeP, nodeN): #carsp = cars going in positive direction; carsn = cars going in negative direction
+    
+    def __init__(self, nodeP, nodeN): #carsp = cars going in positive direction; carsn = cars going in negative direction
         self.nodeP,self.nodeN=nodeP,nodeN
-        self.carsP,self.carsN = carsP,carsN
         self.cars = cars
+        self.carsP,self.carsN=[],[]
         
+    
+#create nodes ---------------
+for y in range(mapsize[0]):
+    for x in range(mapsize[1]):
+        if random.randint(0,2):
+            nodes.append(node([x,y]))
+#print(nodes)
+#print(nodes[0].x,nodes[0].y,n:=next_node(nodes[0],"d"))
+#if n: print(n.x,n.y)
+#create edges ---------------
+for n in range(len(nodes)):
+    #print("Pre-edit: ",n,nodes[n])
+    node=nodes[n]
+    for direction in "udlr":
+        if node.edges[direction] == None:
+            next_node = find_next_node(node,direction)
+            #print(next_node)
+            if next_node:
+                new_edge = edge(node,next_node)
+                edges.append(new_edge)
+                node.edges[direction] = new_edge
+                next_node.edges[inverse_directions[direction]] = new_edge
+            else:
+                node.edges[direction] = False
         
-if __name__=="__main__":
-    for y in range(mapsize[0]):
-        for x in range(mapsize[1]):
-            if random.randint(0,2):
-                nodes.append(node([x,y]))
-    
-    
-    print(nodes)
-    for node in nodes:
-        pass
-        #i
-        #edges.append(
-            
+    #print("Post-edit: ",node)
+#print(edges)
+
+#print (get_neighbor(nodes[5]))
